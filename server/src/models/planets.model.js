2f88910f6eb82c26330dd7e12216872587ebbfe9
Ahmad358 <ahmdkhan.cs@gmail.com>
2022-07-08 22:26:28 +0500

    Initial commit 🚀

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const {parse} = require('csv-parse');
+const planet = require('./planets.mongo');
+
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === "CONFIRMED" 
+        && planet['koi_insol'] > 0.36 
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData(){
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true
+        }))
+        .on('data', async (data) => {
+            if(isHabitablePlanet(data)){
+                savePlanet(data)
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', async () => {
+            const countPlanetsFound = (await getAllPlanets()).length;
+            console.log(`Found ${countPlanetsFound} habitable planets.`);
+            resolve();
+        });
+    });
+}
+
+async function getAllPlanets(){
+    return await planet.find({},{
+        "_id": 0,
+        "__v": 0
+    });
+}
+
+async function savePlanet(data){
+    try{
+        await planet.updateOne({
+            keplerName: data.kepler_name
+        }, {
+            keplerName: data.kepler_name
+        }, {
+            upsert: true
+        });
+    }catch(err){
+        console.log(`Could not save planet ${err}`);
+    }
+}
+
+module.exports = {
+    loadPlanetsData,
+    getAllPlanets
+};
